0422781397153d5ac4e203b5700773611792fd74
vineeth <119692597+vineeth38@users.noreply.github.com>
2025-11-14 15:19:58 +0530

    settings.py

--- auth_proj/settings.py
+++ auth_proj/settings.py
@@ -133,13 +133,13 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
+ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
