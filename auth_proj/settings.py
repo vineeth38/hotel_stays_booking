ef93e5e22d2c2bab98fcf17fce04645b8e7452b4
vineeth <119692597+vineeth38@users.noreply.github.com>
2025-11-14 15:07:15 +0530

    settings.py

--- auth_proj/settings.py
+++ auth_proj/settings.py
@@ -82,13 +82,13 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': {
+     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'auth',
-        'USER':'root',
-        'PASSWORD':'Vineeth@12',
-        'HOST':'localhost',
-        'PORT':'3306'
+        'NAME':env("Database_name"),
+        'USER':env("User"),
+        'PASSWORD':env("Password"),
+        'HOST':env("Host"),
+        'PORT':env("Port")
     }
 }
 
